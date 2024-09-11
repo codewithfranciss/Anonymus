6dea9cf534b0a01203a69fc5ed79d8166334d4e1
winnerezy <eragbonikpeya@gmail.com>
2024-09-11 16:33:56 +0100

    chore: Landing page refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,97 @@
-'use client'
-import { useEffect, useState } from "react";
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { LuClipboard } from "react-icons/lu";
+import { LuClipboardCheck } from "react-icons/lu";
+import Typed from "typed.js";
 
 export default function Home() {
-  const fullText = "Welcome to Anonymous";
-  const [displayedText, setDisplayedText] = useState('');
-  const [isTypingComplete, setIsTypingComplete] = useState(false);
+  const [roomName, setRoomName] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [generatedLink, setGeneratedLink] = useState('');
+  const [generatedLink, setGeneratedLink] = useState("");
   const [isCopied, setIsCopied] = useState(false);
 
+  const el = useRef<HTMLParagraphElement | null>(null);
   useEffect(() => {
-    let currentIndex = 0;
-    const typingInterval = setInterval(() => {
-      setDisplayedText((prev) => prev + fullText[currentIndex]);
-      currentIndex++;
+    const typed = new Typed(el.current, {
+      strings: ["Welcome to Anonymous"],
+      typeSpeed: 50,
+    });
 
-      if (currentIndex === fullText.length) {
-        clearInterval(typingInterval);
-        setIsTypingComplete(true);
-      }
-    }, 100);
-
-    return () => clearInterval(typingInterval);
+    return () => typed.destroy();
   }, []);
 
   const generateLink = () => {
-    const link = `https://chatroom.example.com/${Math.random().toString(36).substring(7)}`;
+    if (roomName.trim() === "") return;
+    const link = `https://chatroom.example.com/${Math.random()
+      .toString(36)
+      .substring(7)}`;
     setGeneratedLink(link);
   };
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(generatedLink).then(() => {
-      setIsCopied(true);
-      setTimeout(() => {
-        setIsCopied(false);
-      }, 2000); 
-    });
+    navigator.clipboard.writeText(generatedLink);
+    setIsCopied(true);
   };
 
   return (
     <div className="flex flex-col min-h-screen justify-center items-center">
-      <p className="text-4xl font-bold">{displayedText}</p>
+      <p className="text-4xl font-bold" ref={el} />
 
-      {isTypingComplete && (
-        <>
-          <button
-            className="btn mt-6 bg-primary hover:bg-accent text-white"
-            onClick={() => setIsModalOpen(true)}
-          >
-            Create a chatroom
-          </button>
-          {isModalOpen && (
-            <div className="modal modal-open">
-              <div className="modal-box bg-secondary h-96">
-                <div className="flex justify-between items-center">
-                  <h3 className="font-bold text-lg">Create a new chatroom</h3>
-                  <button
-                    className="btn btn-sm btn-circle flex bg-secondary hover:bg-accent text-white"
-                    onClick={() => setIsModalOpen(false)}
-                  >
-                    x
-                  </button>
-                </div>
-                <form action="" className="mt-4 flex flex-col items-center">
-                  <input
-                    type="text"
-                    placeholder="Enter chat room name"
-                    className="input input-bordered w-full max-w-xs bg-secondary"
-                  />
-                  <button
-                    type="button"
-                    className="btn mt-6 bg-primary w-52 hover:bg-accent text-white"
-                    onClick={generateLink}
-                  >
-                    Generate Link
-                  </button>
+      <button
+        className="btn mt-6 bg-primary hover:bg-accent text-white "
+        onClick={() => setIsModalOpen(true)}
+      >
+        Create a chatroom
+      </button>
+      <>
+        {isModalOpen && (
+          <div className="modal modal-open">
+            <div className="modal-box bg-secondary space-y-4 h-[300px]">
+              <div className="flex justify-between items-center">
+                <h3 className="text-2xl font-semibold">
+                  Create a new chatroom
+                </h3>
+                <button
+                  className="btn btn-sm btn-circle flex bg-secondary hover:bg-accent text-white"
+                  onClick={() => setIsModalOpen(false)}
+                >
+                  x
+                </button>
+              </div>
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  placeholder="Enter chat room name"
+                  className="input input-bordered w-full bg-secondary"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setRoomName(e.target.value)
+                  }
+                />
+                <button
+                  className="btn mt-6 bg-primary w-full hover:bg-accent text-white"
+                  onClick={generateLink}
+                >
+                  Generate Link
+                </button>
+
                 {generatedLink && (
-                  <div className="mt-6 flex items-center">
-                    <input
-                      type="text"
-                      value={generatedLink}
-                      readOnly
-                      className="input input-bordered w-full max-w-xs bg-secondary"
-                    />
+                  <div className="mt-6 flex items-center justify-self-end h-full">
+                    <p className="h-12 border rounded-md flex items-center justify-center px-2 w-full font-semibold">
+                      {generatedLink}
+                    </p>
                     <button
                       className="btn ml-2 bg-accent hover:bg-primary text-white"
                       onClick={copyToClipboard}
                     >
-                      Copy
+                      {isCopied ? <LuClipboardCheck /> : <LuClipboard />}
                     </button>
-                    
-                    {isCopied && (
-                      <span className="ml-2 text-green-500">Copied!</span>
-                    )}
                   </div>
                 )}
-                </form>
               </div>
             </div>
-          )}
-        </>
-      )}
+          </div>
+        )}
+      </>
     </div>
   );
 }
